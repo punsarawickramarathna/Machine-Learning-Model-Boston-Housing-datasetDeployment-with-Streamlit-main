8d3d9647906fb134ceb82d21cf503506b21fdc47
punsarawickramarathna <punsarawikramarathna@gmail.com>
2025-08-09 10:51:42 +0530

    change

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import matplotlib.pyplot as plt
+
 import seaborn as sns
+
+
+try:
+    import matplotlib.pyplot as plt
+except ModuleNotFoundError:
+    plt = None
 
 # ---------------------------
 # Load Data & Model
